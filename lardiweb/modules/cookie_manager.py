cf49518b445dfcb535f3c3eaca69b6f21e41b77a
BladeXses21 <arturkopaneckiy11@gmail.com>
2025-06-18 21:51:50 +0300

    work version 0.0.1.2

--- lardiweb/modules/cookie_manager.py
+++ lardiweb/modules/cookie_manager.py
@@ -95,7 +95,7 @@ class CookieManager:
         try:
             # Ініціалізація undetected_chromedriver в безголовому режимі
             options = uc.ChromeOptions()
-            options.add_argument('--headless')  # Запуск без вікна браузера
+            # options.add_argument('--headless')  # Запуск без вікна браузера
             options.add_argument('--no-sandbox')
             options.add_argument('--disable-dev-shm-usage')
             options.add_argument('--disable-gpu')
